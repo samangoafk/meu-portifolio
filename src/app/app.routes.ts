17697ac67e27add19c59612fc30c0dde0bcc3f27
adam castro <riuksilva72@gmail.com>
2026-09-04 15:06:41 -0300

    feat: estrutura de pastas, componentes e rotas da etapa 4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+    path: '',
+    loadComponent: () => import('./features/sobre/sobre').then(m => m.Sobre)
+  },
+    
+  {
+    path: 'sobre',
+    loadComponent: () => import('./features/sobre/sobre').then(m => m.Sobre)
+  },
+  {
+    path: 'formacao',
+    loadComponent: () => import('./features/formacao/formacao').then(m => m.Formacao)
+  },
+  {
+    path: 'habilidades',
+    loadComponent: () => import('./features/habilidades/habilidades').then(m => m.Habilidades)
+  },
+  {
+    path: 'projetos',
+    loadComponent: () => import('./features/projetos/projetos').then(m => m.Projetos)
+  },
+];
